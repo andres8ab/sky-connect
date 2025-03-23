49b67b1682fa7e7a14a3db0addd4e69320b06175
andres8ab <ochoabawab@gmail.com>
2025-03-23 16:40:19 -0500

    update aviationstack address

--- app/airport/[code]/page.tsx
+++ app/airport/[code]/page.tsx
@@ -122,8 +122,8 @@ export default function AirportDetails() {
 
   if (error) {
     return (
-      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-7xl mx-auto">
+      <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto pt-[12vh]">
           <div className="bg-red-50 dark:bg-red-900 border border-red-200 dark:border-red-800 rounded-lg p-4">
             <p className="text-red-700 dark:text-red-300">
               No se ha encontrado información del aeropuerto
@@ -136,8 +136,8 @@ export default function AirportDetails() {
 
   if (!airport) {
     return (
-      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-7xl mx-auto">
+      <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto pt-[12vh]">
           <div className="bg-red-50 dark:bg-red-900 border border-red-200 dark:border-red-800 rounded-lg p-4">
             <p className="text-red-700 dark:text-red-300">
               {"Airport not found"}
